60b0315d781d245168da92d919b885d35857ca8d
justinli007 <justinli2432@gmail.com>
2024-06-09 20:45:01 -0400

    Add html parsing and link freq counter

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict"
+
+import { argv } from "node:process";
+import { crawlPage } from "./crawl.js";
+import { printReport } from "./report.js";
+
+async function main() {
+  if (argv.length !== 3) {
+    console.log(`Usage: node main.js <baseURL>`);
+    return
+  }
+
+  const baseURL = argv[2];
+  const result = await crawlPage(baseURL);  
+  printReport(result);
+}
+
+await main();
---
+++ report.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function printReport(pages) {
+  console.log("\nGenerating report...");
+  const sortedPages = sortPages(pages);  
+  for (const page of sortedPages) {
+    const url = page[0];
+    const count = page[1];
+    console.log(`Found ${count} internal links to ${url}`);
+  }
+}
+
+function sortPages(pages) {
+  const sortedPages = Object.entries(pages);
+  sortedPages.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0].localeCompare(b[0]);
+    }
+    return b[1] - a[1];
+  });
+  return sortedPages;
+}
+
+export { printReport, sortPages };
