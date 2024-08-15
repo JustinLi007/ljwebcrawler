fcc292dcfb76a1f0ee23bcc0f373983cf2f6c58d
justinli007 <justinli2432@gmail.com>
2024-08-15 17:35:26 -0400

    Format code

--- crawl.js
+++ crawl.js
@@ -3,94 +3,94 @@
 import { JSDOM } from "jsdom";
 
 function normalizeURL(urlString) {
-  const urlObj = new URL(urlString);
-  const hostname = urlObj.hostname;
-  const pathname = urlObj.pathname;
-  const normalizedUrl = `${hostname}${pathname.replace(/\/$/, "")}`;
+    const urlObj = new URL(urlString);
+    const hostname = urlObj.hostname;
+    const pathname = urlObj.pathname;
+    const normalizedUrl = `${hostname}${pathname.replace(/\/$/, "")}`;
 
-  return normalizedUrl;
+    return normalizedUrl;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
-  const dom = new JSDOM(htmlBody);
-  const anchorElements = dom.window.document.querySelectorAll("a");
-  const urls = []
+    const dom = new JSDOM(htmlBody);
+    const anchorElements = dom.window.document.querySelectorAll("a");
+    const urls = []
 
-  anchorElements.forEach((anchor) => {
-    if (anchor.hasAttribute("href")) {
-      const href = anchor.getAttribute("href");
-      try {
-        const url = new URL(href, baseURL);
-        urls.push(url.href);
-      } catch(err) {
-        console.log(`Error getURLsFromHTML: ${err.message} - ${href}`);
-      }
-    }
-  });
+    anchorElements.forEach((anchor) => {
+        if (anchor.hasAttribute("href")) {
+            const href = anchor.getAttribute("href");
+            try {
+                const url = new URL(href, baseURL);
+                urls.push(url.href);
+            } catch (err) {
+                console.log(`Error getURLsFromHTML: ${err.message} - ${href}`);
+            }
+        }
+    });
 
-  return urls;
+    return urls;
 }
 
-async function crawlPage(rootURL, currentURL=rootURL, pages={}) {
-  const rootURLDomain = getURLDomain(rootURL);
-  const currentURLDomain = getURLDomain(currentURL);
-  if (rootURLDomain !== currentURLDomain) {
-    return pages;
-  }
-
-  const currentURLNormalized = normalizeURL(currentURL);
-  if (currentURLNormalized in pages) {
-    pages[currentURLNormalized]++;
-    return pages;
-  }
-  pages[currentURLNormalized] = 1;
-
-  const html = await fetchPageHTML(currentURL);
-  if (html !== null) {
-    console.log(`Crawling ${currentURL}`);
-    const urls = getURLsFromHTML(html, currentURL);
-    for (const url of urls) {
-      await crawlPage(rootURL, url, pages); 
+async function crawlPage(rootURL, currentURL = rootURL, pages = {}) {
+    const rootURLDomain = getURLDomain(rootURL);
+    const currentURLDomain = getURLDomain(currentURL);
+    if (rootURLDomain !== currentURLDomain) {
+        return pages;
     }
-  }
 
-  return pages;
+    const currentURLNormalized = normalizeURL(currentURL);
+    if (currentURLNormalized in pages) {
+        pages[currentURLNormalized]++;
+        return pages;
+    }
+    pages[currentURLNormalized] = 1;
+
+    const html = await fetchPageHTML(currentURL);
+    if (html !== null) {
+        console.log(`Crawling ${currentURL}`);
+        const urls = getURLsFromHTML(html, currentURL);
+        for (const url of urls) {
+            await crawlPage(rootURL, url, pages);
+        }
+    }
+
+    return pages;
 }
 
 async function fetchPageHTML(rootURL) {
-  let response = null;
-  try {
-    response = await fetch(rootURL);
-  } catch(err) {
-    console.log(`Error fetchPageHTML: ${err.message} - ${rootURL}`);
-    return null
-  }
-  
-  if (response.status >= 400) {
-    console.log(`Failed to fetch ${rootURL}`);
-    return null
-  } 
+    let response = null;
+    try {
+        response = await fetch(rootURL);
+    } catch (err) {
+        console.log(`Error fetchPageHTML: ${err.message} - ${rootURL}`);
+        return null
+    }
 
-  const contentType = response.headers.get("content-type");
-  if (!contentType || !contentType.includes("text/html")) {
-    console.log(`Content is not HMTL: ${rootURL} - ${contentType}`);
-    return null
-  }
+    if (response.status >= 400) {
+        console.log(`Failed to fetch ${rootURL}`);
+        return null
+    }
 
-  return await response.text();
+    const contentType = response.headers.get("content-type");
+    if (!contentType || !contentType.includes("text/html")) {
+        console.log(`Content is not HMTL: ${rootURL} - ${contentType}`);
+        return null
+    }
+
+    return await response.text();
 }
 
 function getURLDomain(url) {
-  if (!url) {
-    return null
-  }
+    if (!url) {
+        return null
+    }
 
-  const urlObj = new URL(url);
-  const hostname = urlObj.hostname;
-  const parts = hostname.split(".");
-  const domain = parts.slice(parts.length-2).join(".");
+    const urlObj = new URL(url);
+    const hostname = urlObj.hostname;
+    const parts = hostname.split(".");
+    const domain = parts.slice(parts.length - 2).join(".");
 
-  return domain;
+    return domain;
 }
 
 export { normalizeURL, getURLsFromHTML, crawlPage };
--- main.js
+++ main.js
@@ -5,14 +5,14 @@ import { crawlPage } from "./crawl.js";
 import { printReport } from "./report.js";
 
 async function main() {
-  if (argv.length !== 3) {
-    console.log(`Usage: node main.js <baseURL>`);
-    return
-  }
+    if (argv.length !== 3) {
+        console.log(`Usage: node main.js <baseURL>`);
+        return
+    }
 
-  const baseURL = argv[2];
-  const result = await crawlPage(baseURL);  
-  printReport(result);
+    const baseURL = argv[2];
+    const result = await crawlPage(baseURL);
+    printReport(result);
 }
 
 await main();
